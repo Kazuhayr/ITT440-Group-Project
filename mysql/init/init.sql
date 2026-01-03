5eeb3fbda6b70e9e93a8a2fb49776874c6a9fa2c
Kazuhayr <mohammedzuhayr118@gmail.com>
2026-01-04 01:49:11 +0800

    edit

---
+++ mysql/init/init.sql
@@ -0,0 +1,12 @@
+USE pointsdb;
+
+CREATE TABLE IF NOT EXISTS points_table (
+    user VARCHAR(50) PRIMARY KEY,
+    points INT DEFAULT 0,
+    datetime_stamp DATETIME
+);
+
+INSERT INTO points_table (user, points, datetime_stamp)
+VALUES
+('python_user', 0, NOW()),
+('c_user', 0, NOW());
